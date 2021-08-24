9665edb479c22679820315cc9e82a43abde3f2bc
orlafoley <57141087+orlafoley@users.noreply.github.com>
2021-08-24 19:59:45 +0100

    Interfaces used in assignment 2

---
+++ Book.java
@@ -0,0 +1,7 @@
+public interface Book {
+    public double getPrice( );
+    public String getAuthor( );
+    public double getProfit( );
+    public double getDuration( );
+    public Author returnAuthor( );
+}
---
+++ Person.java
@@ -0,0 +1,6 @@
+public interface Person {
+    public void buy( Book book );
+    public void receive( double money );
+    public void charge( double money );
+    public void printBooksOwned( );
+}
